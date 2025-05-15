816968636116a7f1452781868d3709b941f3222a
zdxiaoda <xiaoda@duck.com>
2025-05-15 22:55:08 +0800

    清除代码

--- predict.py
+++ predict.py
@@ -11,8 +11,17 @@ from PIL import Image
 from llava.utils import disable_torch_init
 from llava.conversation import conv_templates
 from llava.model.builder import load_pretrained_model
-from llava.mm_utils import tokenizer_image_token, process_images, get_model_name_from_path
-from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
+from llava.mm_utils import (
+    tokenizer_image_token,
+    process_images,
+    get_model_name_from_path,
+)
+from llava.constants import (
+    IMAGE_TOKEN_INDEX,
+    DEFAULT_IMAGE_TOKEN,
+    DEFAULT_IM_START_TOKEN,
+    DEFAULT_IM_END_TOKEN,
+)
 
 
 def predict(args):
@@ -20,22 +29,29 @@ def predict(args):
     # to read generation parameters from args
     model_path = os.path.expanduser(args.model_path)
     generation_config = None
-    if os.path.exists(os.path.join(model_path, 'generation_config.json')):
-        generation_config = os.path.join(model_path, '.generation_config.json')
-        os.rename(os.path.join(model_path, 'generation_config.json'),
-                  generation_config)
+    if os.path.exists(os.path.join(model_path, "generation_config.json")):
+        generation_config = os.path.join(model_path, ".generation_config.json")
+        os.rename(os.path.join(model_path, "generation_config.json"), generation_config)
 
     # Load model
     disable_torch_init()
     model_name = get_model_name_from_path(model_path)
-    tokenizer, model, image_processor, context_len = load_pretrained_model(model_path, args.model_base, model_name, device="mps")
+    tokenizer, model, image_processor, context_len = load_pretrained_model(
+        model_path, args.model_base, model_name, device="cuda"
+    )
 
     # Construct prompt
     qs = args.prompt
     if model.config.mm_use_im_start_end:
-        qs = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + qs
+        qs = (
+            DEFAULT_IM_START_TOKEN
+            + DEFAULT_IMAGE_TOKEN
+            + DEFAULT_IM_END_TOKEN
+            + "\n"
+            + qs
+        )
     else:
-        qs = DEFAULT_IMAGE_TOKEN + '\n' + qs
+        qs = DEFAULT_IMAGE_TOKEN + "\n" + qs
     conv = conv_templates[args.conv_mode].copy()
     conv.append_message(conv.roles[0], qs)
     conv.append_message(conv.roles[1], None)
@@ -45,10 +61,14 @@ def predict(args):
     model.generation_config.pad_token_id = tokenizer.pad_token_id
 
     # Tokenize prompt
-    input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).to(torch.device("mps"))
+    input_ids = (
+        tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt")
+        .unsqueeze(0)
+        .to(torch.device("cuda"))
+    )
 
     # Load and preprocess image
-    image = Image.open(args.image_file).convert('RGB')
+    image = Image.open(args.image_file).convert("RGB")
     image_tensor = process_images([image], image_processor, model.config)[0]
 
     # Run inference
@@ -62,22 +82,29 @@ def predict(args):
             top_p=args.top_p,
             num_beams=args.num_beams,
             max_new_tokens=256,
-            use_cache=True)
+            use_cache=True,
+        )
 
-        outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0].strip()
+        outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[
+            0
+        ].strip()
         print(outputs)
 
     # Restore generation config
     if generation_config is not None:
-        os.rename(generation_config, os.path.join(model_path, 'generation_config.json'))
+        os.rename(generation_config, os.path.join(model_path, "generation_config.json"))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model-path", type=str, default="./llava-v1.5-13b")
     parser.add_argument("--model-base", type=str, default=None)
-    parser.add_argument("--image-file", type=str, default=None, help="location of image file")
-    parser.add_argument("--prompt", type=str, default="Describe the image.", help="Prompt for VLM.")
+    parser.add_argument(
+        "--image-file", type=str, default=None, help="location of image file"
+    )
+    parser.add_argument(
+        "--prompt", type=str, default="Describe the image.", help="Prompt for VLM."
+    )
     parser.add_argument("--conv-mode", type=str, default="qwen_2")
     parser.add_argument("--temperature", type=float, default=0.2)
     parser.add_argument("--top_p", type=float, default=None)
